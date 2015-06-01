b1abba5b95d1b65ba2a59c2ac36ce5f10d49294b
ParrisSikorski <pm_sikorski@yahoo.com>
2015-06-01 12:54:06 -0700

    added delete function

--- TDL.js
+++ TDL.js
@@ -20,67 +20,97 @@ function populate_todo_list() {
     $('#display_list').empty();
     for (var i = 0; i < todo_items_array.length; i++) {
         var TD_item = $("<div>", {
-            class: 'TD_item col-xs-12 col-md-12',
-            id: i,
+            class: 'TD_item col-xs-12 col-md-12 row',
+            id: todo_items_array[i].id,
+            data_index: i
+        });
+
+        var delete_button = $("<button>", {
+            type: 'button',
+            class: 'button glyphicon glyphicon-remove-sign',
             data_index: i
         });
         var list_item_num = $("<div>", {
-            class: 'list_item_num',
-            text: "list item number: " + i
+            class: 'list_item_num col-xs-2 col-sm-2 col-md-2 col-lg-2 page_font',
+            text: "list item number: " + (i + 1)
         });
 
         var timestamp = $("<div>", {
-            class: 'to_do_timestamp',
+            class: 'to_do_timestamp col-xs-2 col-sm-2 col-md-2 col-lg-2 page_font',
             text: "time: " + todo_items_array[i].timeStamp,
         });
 
         var title = $("<div>", {
-            class: 'to_do_title',
+            class: 'to_do_title col-xs-2 col-sm-2 col-md-2 col-lg-2 page_font',
             text: "title: " + todo_items_array[i].title,
         });
 
         var details = $("<div>", {
-            class: 'to_do_details',
+            class: 'to_do_details col-xs-2 col-sm-2 col-md-2 col-lg-2 page_font',
             text: "details: " + todo_items_array[i].details,
         });
 
-        $(TD_item).append(list_item_num, title, details, timestamp);
+        $(TD_item).append(list_item_num, title, details, timestamp, delete_button);
         $('#display_list').append(TD_item);
+
+        delete_button.click(function() {
+            var index = $(this).parent().attr('data_index');
+            console.log("student ", index + ' was clicked');
+            console.log("student ", index + " was deleted");
+            todo_items_array.splice(index, 1);
+            populate_todo_list();
+        });
     }
 }
 
 function populate_todo_single() {
+
     var iLN = parseFloat($('#id_LI').val());
     console.log("item list number chosen: ", iLN);
     console.log("todo_items_array: ", todo_items_array);
-    for (var i = iLN; i < (iLN+1); i++) {
+    for (var i = iLN; i < (iLN + 1); i++) {
         var TD_item = $("<div>", {
-            class: 'TD_item col-xs-12 col-md-12',
-            id: (i-1),
-            data_index: (i-1)
+            class: 'TD_item col-xs-12 col-md-12 row',
+            id: todo_items_array[i - 1].id,
+            data_index: (i - 1)
+        });
+
+        var delete_button = $("<button>", {
+            type: 'button',
+            class: 'button glyphicon glyphicon-remove-sign',
+            data_index: i
         });
         var list_item_num = $("<div>", {
-            class: 'list_item_num',
+            class: 'list_item_num col-xs-2 col-sm-2 col-md-2 col-lg-2 page_font',
             text: "list item number: " + (i)
         });
 
         var timestamp = $("<div>", {
-            class: 'to_do_timestamp',
-            text: "time: " + todo_items_array[i-1].timeStamp,
+            class: 'to_do_timestamp col-xs-2 col-sm-2 col-md-2 col-lg-2 page_font',
+            text: "time: " + todo_items_array[i - 1].timeStamp,
         });
 
         var title = $("<div>", {
-            class: 'to_do_title',
-            text: "title: " + todo_items_array[i-1].title,
+            class: 'to_do_title col-xs-2 col-sm-2 col-md-2 col-lg-2 page_font',
+            text: "title: " + todo_items_array[i - 1].title,
         });
 
         var details = $("<div>", {
-            class: 'to_do_details',
-            text: "details: " + todo_items_array[i-1].details,
+            class: 'to_do_details col-xs-2 col-sm-2 col-md-2 col-lg-2 page_font',
+            text: "details: " + todo_items_array[i - 1].details,
         });
 
-        $(TD_item).append(list_item_num, title, details, timestamp);
+        $(TD_item).append(list_item_num, title, details, timestamp, delete_button);
         $('#display_list').append(TD_item);
+
+        delete_button.click(function() {
+            var index = $(this).parent().attr('data_index');
+            console.log("student ", index + ' was clicked');
+            console.log("student ", index + " was deleted");
+            todo_items_array.splice(index, 1);
+            var index = $(this).parent().remove();
+            //populate_todo_single();
+        });
     }
 }
 
