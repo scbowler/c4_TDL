cccac54dfdb8e297f14778385c50ff8c57e54bb9
Jordan <jhealth7@gmail.com>
2015-06-02 16:47:00 -0700

    updated ajax logout

--- ajax.js
+++ ajax.js
@@ -1,4 +1,36 @@
-$.ajax({
-	url:'http://learningfuze.com/todo/login',
+function user_login() {
+    $.ajax({
+        url: 'http://s-apis.learningfuze.com/todo/login',
+        dataType: 'json',
+        cache: false,
+        method: 'POST',
+        crossDomain: true,
+        data: {
+            username: , 
+            password: 
+        }
+        success: function(response) {
+            console.log("response ", response);
+        }
 
-})
+    });
+
+}
+
+function logout(){
+
+	$.ajax({
+		url:'http://s-apis.learningfuze.com/todo/logout',
+		dataType: 'json',
+		cache: false,
+		crossDomain: true,
+		success: function(response){
+			console.log("response ", response);
+			user_object = {};
+		}
+		error: function(response){
+			console.log("response ", response);
+
+		}
+	});
+}
